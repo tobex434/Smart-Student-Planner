261bdd49eeeb28f54ac2b956f1cf6f9ab1ad8549
“Sodiq” <bankole434@gmail.com>
2026-05-12 22:07:05 +0100

    added comments to the code

--- lib/views/screens/main_shell.dart
+++ lib/views/screens/main_shell.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'dashboard_screen.dart';
 import 'tasks_screen.dart';
 import 'timer_screen.dart';
-import 'settings_screen.dart';   // settings replaces profile
+import 'settings_screen.dart';
 
+//the mainshell of the app serves as the navigation hub
+// a ststeful widget is used because we need to track the current tab that is active
 class MainShell extends StatefulWidget {
   const MainShell({super.key});
 
@@ -12,8 +14,11 @@ class MainShell extends StatefulWidget {
 }
 
 class _MainShellState extends State<MainShell> {
+  //sets the current active tab to 0 which is the dashboard screen
   int _currentIndex = 0;
 
+// a list of screens that we can navigate to using the bottom navigation bar
+// const is used to mkae the list immutable and improve performance
   final List<Widget> _screens = const [
     DashboardScreen(),
     TasksScreen(),
@@ -27,7 +32,8 @@ class _MainShellState extends State<MainShell> {
       body: _screens[_currentIndex],
 
       bottomNavigationBar: NavigationBar(
-        selectedIndex: _currentIndex,
+        selectedIndex: _currentIndex, // highlights the current active tab which is the dashboard screen
+        //when a user taps a tab, it trigger a ui refresh by calling setstate and updates the current index to the index of the tapped tab
         onDestinationSelected: (index) {
           setState(() => _currentIndex = index);
         },
