4fd3dc8dee0fdb487767c2f2d629c24b9ef8624c
“Sodiq” <bankole434@gmail.com>
2026-05-12 21:51:37 +0100

    feat: added dashboard screen placeholder

---
+++ lib/views/screens/dashboard_screen.dart
@@ -0,0 +1,13 @@
+// dashboard_screen.dart
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Dashboard')),
+    );
+  }
+}
