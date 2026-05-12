2327049c89c5cd0368bf53ccc3e017debd6d7879
“Sodiq” <bankole434@gmail.com>
2026-05-12 21:52:21 +0100

    feat: added settings screen placeholder

---
+++ lib/views/screens/settings_screen.dart
@@ -0,0 +1,13 @@
+// settings_screen.dart
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Settings')),
+    );
+  }
+}
