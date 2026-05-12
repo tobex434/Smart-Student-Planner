f1cf7daba76f83c7c1e7beacbcce6d3095945d62
“Sodiq” <bankole434@gmail.com>
2026-05-12 21:52:42 +0100

    feat: added task screen placeholder

---
+++ lib/views/screens/tasks_screen.dart
@@ -0,0 +1,13 @@
+// tasks_screen.dart
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Tasks')),
+    );
+  }
+}
