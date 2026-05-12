f64a269297e420c3c10310b9120d82b5a282741c
“Sodiq” <bankole434@gmail.com>
2026-05-12 21:54:04 +0100

    feat: added timer screen placeholder

---
+++ lib/views/screens/timer_screen.dart
@@ -0,0 +1,13 @@
+// timer_screen.dart
+import 'package:flutter/material.dart';
+
+class TimerScreen extends StatelessWidget {
+  const TimerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Timer')),
+    );
+  }
+}
